41c47fb675fabba5a8788b4a5429f1c276a38a05
Parham Abolghasemi <parhamab17@gmail.com>
2022-07-22 21:06:25 +0430

    reducing the number of codes lines

--- script.js
+++ script.js
@@ -1,35 +1,31 @@
-// variables
+// elements
 let displayBox = document.querySelector(".display");
 let btn = document.querySelectorAll(".btn");
 const calculate = document.querySelector(".calculate");
 const AC = document.querySelector(".clear-all");
 const C = document.querySelector(".clear-last");
-
 // get all numbers and operators & show them on display box by clicking
 btn.forEach((item) => {
   item.addEventListener("click", function (e) {
-    const x = e.target.innerHTML;
-    if (displayBox.innerHTML == 0) {
-      return (displayBox.innerHTML = x);
-    }
-    return (displayBox.innerHTML += x);
+    const clickedItems = e.target.innerHTML;
+    displayBox.innerHTML == 0
+      ? (displayBox.innerHTML = clickedItems)
+      : (displayBox.innerHTML += clickedItems);
   });
 });
 // calculate button
 calculate.addEventListener("click", function () {
-  let result = displayBox.innerText;
-  displayBox.innerText = eval(result);
+  let currentVal = displayBox.innerText;
+  displayBox.innerText = eval(currentVal);
 });
 // clear all (AC)
 AC.addEventListener("click", function () {
   displayBox.innerHTML = 0;
 });
-// clear last (C)
+// clear last value (C)
 C.addEventListener("click", function () {
-  let txt = displayBox.innerText;
-  if (txt.length === 1) {
-    displayBox.innerText = 0;
-  } else {
-    displayBox.innerText = txt.substring(0, txt.length - 1);
-  }
+  let currentVal = displayBox.innerText;
+  currentVal.length === 1
+    ? (displayBox.innerText = 0)
+    : (displayBox.innerText = currentVal.substring(0, currentVal.length - 1));
 });
